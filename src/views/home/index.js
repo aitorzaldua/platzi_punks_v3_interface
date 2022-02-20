0b43ece790f4ae2eee89ee4792fe90ad9592e9d9
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-02-20 20:25:34 +0100

    200222 add maxSupply in /home

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,9 +1,34 @@
+import { useWeb3React } from "@web3-react/core";
+import { useCallback, useEffect, useState } from "react";
+import usePlatziPunks from "../../hooks/usePlatzi";
 const Home = () => {
+
+    const { active } = useWeb3React();
+    const [maxSupply, setMaxSupply] = useState();
+
+    const platziPunks = usePlatziPunks();
+
+    const getMaxSupply = useCallback(async () => {
+        if (platziPunks) {
+            const result = await platziPunks.methods.maxSupply().call();
+            setMaxSupply(result);
+        }
+    }, [platziPunks]);
+
+    useEffect(() => {
+        getMaxSupply();
+    }, [getMaxSupply]);
+
+    if (!active) return "Please, connect your wallet";
+
+
     return (
         <>
-             <p>Hello, decentralized buddy</p>
+            <p>The maximum of NFTs is: {maxSupply}</p>
         </>
     );
 }
 
 export default Home;
+
+
