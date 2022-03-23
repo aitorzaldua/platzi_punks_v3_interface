7083956748f2ebfc4ffd411c8b891a35015e1020
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-03-23 19:59:19 +0100

    last version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/home';
 import Punks from './views/punks'
-import Punk from './views/punk';
+/* import Punk from './views/punk'; */
 import MainLayout from './layouts/main';
 
 function App() {
